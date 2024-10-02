2f29c80d2725a64d9e5e875c6ebe99d31257068b
Beckversync <118ngobach@gmail.com>
2024-10-02 14:15:21 +0700

    only update function

--- LAB2/Core/Src/main.c
+++ LAB2/Core/Src/main.c
@@ -165,8 +165,8 @@ void update7SEG(int index){
 		HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, SET);
 		HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, SET);
 		HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, SET);
-		// display first value in led_buffer[]
-		display7SEG_1(led_buffer[0]);
+
+
 		break;
 	case 1:
 		// Display the second 7 SEG with led_buffer [1]
@@ -175,8 +175,8 @@ void update7SEG(int index){
 		HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, RESET);
 		HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, SET);
 		HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, SET);
-		// display second value in led_buffer[]
-		display7SEG_1(led_buffer[1]);
+
+
 		break;
 	case 2:
 		// Display the third 7 SEG with led_buffer [2]
@@ -185,8 +185,8 @@ void update7SEG(int index){
 		HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, SET);
 		HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, RESET);
 		HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, SET);
-		// display third value in led_buffer[]
-		display7SEG_1(led_buffer[2]);
+
+
 		break;
 	case 3:
 		// Display the fourth 7 SEG with led_buffer [3]
@@ -195,11 +195,17 @@ void update7SEG(int index){
 		HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, SET);
 		HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, SET);
 		HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, RESET);
-		// display fourth value in led_buffer[]
-		display7SEG_1(led_buffer[3]);
+
+
 		break;
 	default: break;
 	}
+}
+void updateClockBuffer(int hour, int minute){
+	led_buffer[0] = hour / 10;
+	led_buffer[1] = hour % 10;
+	led_buffer[2] = minute/10;
+	led_buffer[3] = minute % 10;
 }
 /* USER CODE END 0 */
 
@@ -238,8 +244,23 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+  int hour = 15 , minute = 8 , second = 50;
   while (1)
   {
+	  second ++;
+	  if(second >= 60){
+		  second = 0;
+		  minute ++;
+	  }
+	  if(minute >= 60){
+		  minute = 0;
+		  hour ++;
+	  }
+	  if(hour >= 24){
+		  hour = 0;
+	  }
+	 updateClockBuffer(hour, minute);
+	  HAL_Delay(1000);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */

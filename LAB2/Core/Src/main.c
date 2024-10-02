b934145a75d2fe7013d7ab6d4217739c62faa776
Beckversync <118ngobach@gmail.com>
2024-10-02 14:00:21 +0700

    Excercise4

--- LAB2/Core/Src/main.c
+++ LAB2/Core/Src/main.c
@@ -370,17 +370,27 @@ static void MX_GPIO_Init(void)
 
 /* USER CODE BEGIN 4 */
 int count =3;
+int dot=2;
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
 
 	if(count > 0){
 			count --;
 			if(count <= 0){
-				count = 50;
+				count = 25;
 
 				update7SEG(index_led);
 				index_led = (index_led + 1) % MAX_LED;
 			}
 		}
+	if (dot>0)
+	{
+		dot--;
+		if (dot<=0)
+		{
+			dot=100;
+			HAL_GPIO_TogglePin(DOT_GPIO_Port, DOT_Pin);
+		}
+	}
 }
 /* USER CODE END 4 */
 

fd90ae5fb348601553d35915f4051e0e0b61252d
Beckversync <118ngobach@gmail.com>
2024-10-03 16:38:39 +0700

    update

--- LAB2/Core/Src/main.c
+++ LAB2/Core/Src/main.c
@@ -165,7 +165,7 @@ void update7SEG(int index){
 		HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, SET);
 		HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, SET);
 		HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, SET);
-
+		display7SEG_1(led_buffer[0]);
 
 		break;
 	case 1:
@@ -175,7 +175,7 @@ void update7SEG(int index){
 		HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, RESET);
 		HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, SET);
 		HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, SET);
-
+		display7SEG_1(led_buffer[1]);
 
 		break;
 	case 2:
@@ -185,7 +185,7 @@ void update7SEG(int index){
 		HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, SET);
 		HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, RESET);
 		HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, SET);
-
+		display7SEG_1(led_buffer[2]);
 
 		break;
 	case 3:
@@ -195,13 +195,14 @@ void update7SEG(int index){
 		HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, SET);
 		HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, SET);
 		HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, RESET);
-
+		display7SEG_1(led_buffer[3]);
 
 		break;
 	default: break;
 	}
 }
-void updateClockBuffer(int hour, int minute){
+int hour = 15, minute = 8, second = 50;
+void updateClockBuffer(){
 	led_buffer[0] = hour / 10;
 	led_buffer[1] = hour % 10;
 	led_buffer[2] = minute/10;
@@ -244,34 +245,36 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  int hour = 15 , minute = 8 , second = 50;
+
   setTimer0(1000);
   while (1)
   {
 	  if(timer0_flag == 1){
 	  		  setTimer0(1000);
+			  second++;
+			  if(second >= 60){
+				  second = 0;
+				  minute++;
+			  }
 
-	  second ++;
-	  if(second >= 60){
-		  second = 0;
-		  minute ++;
-	  }
-	  if(minute >= 60){
-		  minute = 0;
-		  hour ++;
-	  }
-	  if(hour >= 24){
-		  hour = 0;
-	  }
-	 updateClockBuffer(hour, minute);
-	 HAL_GPIO_TogglePin(DOT_GPIO_Port, DOT_Pin);
-	  }
-	  if(timer0_flag==1)
-	  {
-		  setTimer0(25);
-		  update7SEG(index_led);
-		  index_led=(index_led+1)% MAX_LED;
-	  }
+			  if(minute >= 60){
+				  minute = 0;
+				  hour++;
+			  }
+
+			  if(hour >= 24){
+				  hour = 0;
+			  }
+			  updateClockBuffer();
+
+			  // represent four 7seg led
+			  if(index_led >= MAX_LED) index_led = 0;
+			  update7SEG(index_led++);
+
+			  // toggle dot led
+			  HAL_GPIO_TogglePin(DOT_GPIO_Port, DOT_Pin);
+	  	  }
+
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -401,12 +404,10 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
-int count =3;
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
 
 	timer_run();
-
 }
 /* USER CODE END 4 */
 

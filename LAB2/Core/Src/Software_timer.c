65ffd613f9caddae4577e816dad125ee9785d726
Beckversync <118ngobach@gmail.com>
2024-10-03 21:13:16 +0700

    no message

--- LAB2/Core/Src/Software_timer.c
+++ LAB2/Core/Src/Software_timer.c
@@ -5,46 +5,41 @@
  *      Author: Dell
  */
 
+
 #include "software_timer.h"
 
-int timer1_count;
-int timer1_flag;
 
-int timer2_count;
-int timer2_flag;
+int timer0_counter = 0;
+int timer0_flag = 0;
 
-int timer3_count;
-int timer3_flag;
+int timer1_counter = 0;
+int timer1_flag = 0;
 
-void timerRun(){
-	if(timer1_count > 0){
-		timer1_count--;
-		if(timer1_count <= 0){
+int timer_cycle = 10;
+
+void setTimer0(int duration){
+	timer0_counter = duration / timer_cycle;
+	timer0_flag = 0;
+}
+void setTimer1(int duration){
+	timer1_counter = duration / timer_cycle;
+	timer1_flag = 0;
+}
+
+void timer_run0(){
+	if(timer0_counter > 0){
+		timer0_counter--;
+		if(timer0_counter <= 0){
+			timer0_flag = 1;
+		}
+	}
+}
+
+void timer_run1(){
+	if(timer1_counter > 0){
+		timer1_counter--;
+		if(timer1_counter <= 0){
 			timer1_flag = 1;
 		}
 	}
-	if(timer2_count > 0){
-		timer2_count--;
-		if(timer2_count <= 0){
-			timer2_flag = 1;
-		}
-	}
-	if(timer3_count > 0){
-		timer3_count--;
-		if(timer3_count <= 0){
-			timer3_flag = 1;
-		}
-	}
-}
-void setTimer1(int duration){
-	timer1_count = duration;
-	timer1_flag = 0;
-}
-void setTimer2(int duration){
-	timer2_count = duration;
-	timer2_flag = 0;
-}
-void setTimer3(int duration){
-	timer3_count = duration;
-	timer3_flag = 0;
 }
